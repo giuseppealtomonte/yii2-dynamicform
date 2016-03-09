02e2eca47a38d6515de36a9dfd2714db1bef5cac
Giuseppe Altomonte <galtomon@gmail.com>
2016-03-09 12:07:26 +0100

    v2.0.1.4

--- assets/yii2-dynamic-form.js
+++ assets/yii2-dynamic-form.js
@@ -314,6 +314,23 @@
             });
         });
     };
+	
+	var _restoreKrajeeDepdrop = function ($elem) {
+        var configDepdrop = $.extend(true, {}, eval($elem.attr('data-krajee-depdrop')));
+        var inputID = $elem.attr('id');
+        var matchID = inputID.match(regexID);
+
+        if (matchID && matchID.length === 4) {
+            for (index = 0; index < configDepdrop.depends.length; ++index) {
+                var match = configDepdrop.depends[index].match(regexID);
+                if (match && match.length === 4) {
+                    configDepdrop.depends[index] = match[1] + matchID[2] + match[3];
+                }
+            }
+        }
+
+        $elem.depdrop(configDepdrop);
+    };
 
     var _restoreSpecialJs = function(widgetOptions) {
         var widgetOptionsRoot = _getWidgetOptionsRoot(widgetOptions);
@@ -419,21 +436,12 @@
         // "kartik-v/yii2-widget-depdrop"
         var $hasDepdrop = $(widgetOptionsRoot.widgetItem).find('[data-krajee-depdrop]');
         if ($hasDepdrop.length > 0) {
-            $hasDepdrop.each(function() {
-                $(this).removeData().off();
-                $(this).unbind();
-                var configDepdrop = eval($(this).attr('data-krajee-depdrop'));
-                var inputID = $(this).attr('id');
-                var matchID = inputID.match(regexID);
-                if (matchID && matchID.length === 4) {
-                    for (index = 0; index < configDepdrop.depends.length; ++index) {
-                        var match = configDepdrop.depends[index].match(regexID);
-                        if (match && match.length === 4) {
-                            configDepdrop.depends[index] = match[1] + matchID[2] + match[3];
-                        }
-                    }
+            $hasDepdrop.each(function () {
+                if ($(this).data('select2') === undefined) {
+                    $(this).removeData().off();
+                    $(this).unbind();
+                    _restoreKrajeeDepdrop($(this));
                 }
-                $(this).depdrop(configDepdrop);
             });
         }
 
@@ -441,25 +449,38 @@
         var $hasSelect2 = $(widgetOptionsRoot.widgetItem).find('[data-krajee-select2]');
         if ($hasSelect2.length > 0) {
             $hasSelect2.each(function() {
-                var id = $(this).attr('id');
-                var configSelect2 = eval($(this).attr('data-krajee-select2'));
-                $(this).select2('destroy');
-                $.when($('#' + id).select2(configSelect2)).done(initSelect2Loading(id));
-                $('#' + id).on('select2-open', function() {
-                    initSelect2DropStyle(id)
-                });
-                if ($(this).attr('data-krajee-depdrop')) {
-                    $(this).on('depdrop.beforeChange', function(e,i,v) {
-                        var configDepdrop = eval($(this).attr('data-krajee-depdrop'));
-                        var loadingText = (configDepdrop.loadingText)? configDepdrop.loadingText : 'Loading ...';
-                        $('#' + id).select2('data', {text: loadingText});
-                    });
-                    $(this).on('depdrop.change', function(e,i,v,c) {
-                        $('#' + id).select2('val', $('#' + id).val());
-                    });
-                }
-            });
-        }
+                  var id = $(this).attr('id');
+                  var configSelect2 = eval($(this).attr('data-krajee-select2'));
+                if ($(this).data('select2')) {
+                     $(this).select2('destroy');
+                 }
+ 
+                 var configDepdrop = $(this).data('depdrop');
+                 if (configDepdrop) {
+                     configDepdrop = $.extend(true, {}, configDepdrop);
+                     $(this).removeData().off();
+                     $(this).unbind();
+                     _restoreKrajeeDepdrop($(this));
+                 }
+ 
+                 $.when($('#' + id).select2(configSelect2)).done(initSelect2Loading(id, '.select2-container--krajee'));
+ 
+                 var kvClose = 'kv_close_' + id.replace(/\-/g, '_');
+ 
+                 $('#' + id).on('select2:opening', function(ev) {
+                     initSelect2DropStyle(id, kvClose, ev);
+                  });
+               $('#' + id).on('select2:unselect', function() {
+                     window[kvClose] = true;
+                 });
+ 
+                if (configDepdrop) {
+                     var loadingText = (configDepdrop.loadingText) ? configDepdrop.loadingText : 'Loading ...';
+                     initDepdropS2(id, loadingText);
+                  }
+              });
+          }
+                               
     };
 
 })(window.jQuery);
